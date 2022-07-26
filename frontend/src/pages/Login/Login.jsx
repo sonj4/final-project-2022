2fe2834e1e972b115adf2f811b28e79a8051d59b
MrBene1 <femicobren@gmail.com>
2022-07-26 02:25:55 +0200

    Added gallery and rating popup

--- frontend/src/pages/Login/Login.jsx
+++ frontend/src/pages/Login/Login.jsx
@@ -12,7 +12,7 @@ export default function Login() {
                         <input className='loginInput' type="email" name="email" placeholder="Email" required="" />
                         <input className='loginInput' type="password" name="pswd" placeholder="Password" required="" />
                         <input className='loginInput' type="password" name="pswd" placeholder="Repeat Password" required="" />
-                        <button className='loginButton'>Sign up</button>
+                        <button className='loginButton button'>Sign up</button>
                     </form>
                 </div>
                 <div class="login">
@@ -20,7 +20,7 @@ export default function Login() {
                         <label className='loginLabel' for="chk" aria-hidden="true">Login</label>
                         <input className='loginInput' type="email" name="email" placeholder="Email" required="" />
                         <input className='loginInput' type="password" name="pswd" placeholder="Password" required="" />
-                        <button className='loginButton'>Login</button>
+                        <button className='loginButton button'>Login</button>
                     </form>
                 </div>
             </div>
--- frontend/src/pages/Movie/Movie.js
+++ frontend/src/pages/Movie/Movie.js
@@ -1,11 +1,75 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './Movie.css';
+import Modal from "react-modal";
+import ImageGallery from 'react-image-gallery';
+import { Header, Button, Popup, Grid } from 'semantic-ui-react';
+import Box from '@mui/material/Box';
+import Rating from '@mui/material/Rating';
+import Typography from '@mui/material/Typography';
+
+Modal.setAppElement("#root");
+
+
+
 
 function Movie() {
+    const [value, setValue] = React.useState();
+
+    const [isOpen, setIsOpen] = useState(false);
+
+    function toggleModal() {
+        setIsOpen(!isOpen);
+    }
+
+    const images = [
+        {
+            original: 'https://m.media-amazon.com/images/M/MV5BMThlOWE3MWEtZjM4Ny00M2FiLTkyMmYtZGY3ZTcyMzM5YmNlXkEyXkFqcGdeQWpnYW1i._V1_.jpg',
+            thumbnail: 'https://m.media-amazon.com/images/M/MV5BMThlOWE3MWEtZjM4Ny00M2FiLTkyMmYtZGY3ZTcyMzM5YmNlXkEyXkFqcGdeQWpnYW1i._V1_.jpg'
+        },
+        {
+            original: 'https://m.media-amazon.com/images/M/MV5BMThlOWE3MWEtZjM4Ny00M2FiLTkyMmYtZGY3ZTcyMzM5YmNlXkEyXkFqcGdeQWpnYW1i._V1_.jpg',
+            thumbnail: 'https://m.media-amazon.com/images/M/MV5BMThlOWE3MWEtZjM4Ny00M2FiLTkyMmYtZGY3ZTcyMzM5YmNlXkEyXkFqcGdeQWpnYW1i._V1_.jpg'
+        },
+        {
+            original: 'https://m.media-amazon.com/images/M/MV5BMThlOWE3MWEtZjM4Ny00M2FiLTkyMmYtZGY3ZTcyMzM5YmNlXkEyXkFqcGdeQWpnYW1i._V1_.jpg',
+            thumbnail: 'https://m.media-amazon.com/images/M/MV5BMThlOWE3MWEtZjM4Ny00M2FiLTkyMmYtZGY3ZTcyMzM5YmNlXkEyXkFqcGdeQWpnYW1i._V1_.jpg'
+        },
+        {
+            original: 'https://m.media-amazon.com/images/M/MV5BMThlOWE3MWEtZjM4Ny00M2FiLTkyMmYtZGY3ZTcyMzM5YmNlXkEyXkFqcGdeQWpnYW1i._V1_.jpg',
+            thumbnail: 'https://m.media-amazon.com/images/M/MV5BMThlOWE3MWEtZjM4Ny00M2FiLTkyMmYtZGY3ZTcyMzM5YmNlXkEyXkFqcGdeQWpnYW1i._V1_.jpg'
+        },
+        {
+            original: 'https://m.media-amazon.com/images/M/MV5BMThlOWE3MWEtZjM4Ny00M2FiLTkyMmYtZGY3ZTcyMzM5YmNlXkEyXkFqcGdeQWpnYW1i._V1_.jpg',
+            thumbnail: 'https://m.media-amazon.com/images/M/MV5BMThlOWE3MWEtZjM4Ny00M2FiLTkyMmYtZGY3ZTcyMzM5YmNlXkEyXkFqcGdeQWpnYW1i._V1_.jpg'
+        },
+        {
+            original: 'https://m.media-amazon.com/images/M/MV5BMThlOWE3MWEtZjM4Ny00M2FiLTkyMmYtZGY3ZTcyMzM5YmNlXkEyXkFqcGdeQWpnYW1i._V1_.jpg',
+            thumbnail: 'https://m.media-amazon.com/images/M/MV5BMThlOWE3MWEtZjM4Ny00M2FiLTkyMmYtZGY3ZTcyMzM5YmNlXkEyXkFqcGdeQWpnYW1i._V1_.jpg'
+        },
+        {
+          original: 'https://picsum.photos/id/1018/1000/600/',
+          thumbnail: 'https://picsum.photos/id/1018/250/150/',
+        },
+        {
+          original: 'https://picsum.photos/id/1015/1000/600/',
+          thumbnail: 'https://picsum.photos/id/1015/250/150/',
+        },
+        {
+          original: 'https://picsum.photos/id/1019/1000/600/',
+          thumbnail: 'https://picsum.photos/id/1019/250/150/',
+        },
+        
+      ];
+
     return ( <div>
         <div className='container'>
         
-            <img style={{'width': '100%'}} src='https://m.media-amazon.com/images/M/MV5BMThlOWE3MWEtZjM4Ny00M2FiLTkyMmYtZGY3ZTcyMzM5YmNlXkEyXkFqcGdeQWpnYW1i._V1_.jpg' alt='image'/>
+            <img className='gallery-button' src='https://m.media-amazon.com/images/M/MV5BMThlOWE3MWEtZjM4Ny00M2FiLTkyMmYtZGY3ZTcyMzM5YmNlXkEyXkFqcGdeQWpnYW1i._V1_.jpg' alt='image' onClick={toggleModal}/>
+            
+            <Modal isOpen={isOpen} onRequestClose={toggleModal} className="gallery-modal" overlayClassName="modal-overlay">
+                <ImageGallery items={images} style={{all: 'unset'}}/>
+            </Modal>
+
             <div>
                 <div className='categories-x-rating'>
                     <div className='categories'>
@@ -13,8 +77,24 @@ function Movie() {
                         <button>Drama</button>
                     </div>
                     <div>
-                        <p className='rating'>Rating</p>
-                        <p className='rating'>9/10</p>
+                        <Popup className='rate-popup' trigger={<div className='triger'>
+                            <p className='rating'>Rating</p>
+                            <p className='rating'>9/10</p></div>} flowing hoverable>
+                                <Header as='h4'>Rate</Header>
+                                <Box
+                                    sx={{
+                                        '& > legend': { mt: 2 },
+                                    }}
+                                    >
+                                    <Rating
+                                        max={10}
+                                        value={value}
+                                        onChange={(event, newValue) => {
+                                        setValue(newValue);
+                                        }}
+                                    />
+                                </Box>
+                        </Popup>
                     </div>
                 </div>
                 <button className='watch-trailers-link'>Watch trailers</button>
@@ -29,12 +109,40 @@ function Movie() {
                 </div>
             </div>
         </div>
-        <div className='about'>
+        <div className='about hr'>
             <h2>About</h2>
             <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, 
                 when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into 
                 electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, 
                 and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.</p>
+        </div>
+
+        <div className='container2'>
+            <div>
+                <h1 className='actor-title'>Actors</h1>
+                <div className='actors'>
+                    <button>
+                        <img src='https://netflixjunkie.com/wp-content/uploads/2022/06/tommy-shelby-cillian-murphy-peaky-blinders-1569234705-1140x599.jpg' alt='actor1'></img>
+                        <h2>Actor name</h2>
+                        <h3>Act name</h3>
+                    </button>
+                    <button>
+                        <img src='https://netflixjunkie.com/wp-content/uploads/2022/06/tommy-shelby-cillian-murphy-peaky-blinders-1569234705-1140x599.jpg' alt='actor1'></img>
+                        <h2>Actor name</h2>
+                        <h3>Act name</h3>
+                    </button>
+                    <button>
+                        <img src='https://netflixjunkie.com/wp-content/uploads/2022/06/tommy-shelby-cillian-murphy-peaky-blinders-1569234705-1140x599.jpg' alt='actor1'></img>
+                        <h2>Actor name</h2>
+                        <h3>Act name</h3>
+                    </button>
+                    
+                </div>
+                <div className='load-more-div'>
+                    <button className='load-more-button'>Load more</button>
+                </div>
+            </div>
+            <div></div>
         </div>
     </div> );
 }
