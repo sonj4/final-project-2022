57c2bce9125eb936a40e69ec2cfaa1a3a1cbe6b5
Sonja Knezevic <sonjaknezevic2000@gmail.com>
2022-08-03 01:17:41 +0200

    Made search bar to be functional based on the selected option on which search will work

--- frontend/src/components/SearchBar/SearchBar.jsx
+++ frontend/src/components/SearchBar/SearchBar.jsx
@@ -19,14 +19,47 @@ export default function SearchBar() {
         title: "interstellar"
     }]
 
+    const tvShowsData = [{
+        title: "stranger things"
+    }, {
+        title: "peaky blinders"
+    }, {
+        title: "squid game"
+    }, {
+        title: "twin peaks"
+    }, {
+        title: "breaking bad"
+    }, {
+        title: "the offiCe"
+    }]
+
+    const celebData = [{
+        title: "Millie Bobby Brown"
+    }, {
+        title: "Noah Schnapp"
+    }, {
+        title: "Fin Wolfhard"
+    }, {
+        title: "Jimmy Fallon"
+    }, {
+        title: "Harry Styles"
+    }, {
+        title: "Tom Oddel"
+    }]
+
     const [filteredData, setFilteredData] = useState([]);
     const [wordEntered, setWordEntered] = useState("");
     const [isOnFocus, setIsOnFocus] = useState(false);
+    const [searchValue, setSearchValue] = useState("");
 
     const handleFilter = (event) => {
         const searchWord = event.target.value;
         setWordEntered(searchWord);
-        const newFilter = movieData.filter((value) => {
+        let data = [];
+        if (searchValue === "movies") data = [...movieData];
+        else if (searchValue === "tvshows") data = [...tvShowsData];
+        else if (searchValue === "celebs") data = [...celebData];
+        const newFilter = data.filter((value) => {
             return value.title.toLowerCase().includes(searchWord.toLowerCase());
         });
 
@@ -45,8 +78,8 @@ export default function SearchBar() {
     return (
         <div className="input-container">
             <div className="searchMainContainer">
-                <select className='searchBySelect'>
-                    <option value={"none"} disabled selected>Search by</option>
+                <select className='searchBySelect' onChange={e => {setSearchValue(e.target.value); console.log(e.target.value)}}>
+                    <option value={"none"}  defaultValue={true}>Search by</option>
                     <option value={"movies"}>Movies</option>
                     <option value={"tvshows"}>TV shows</option>
                     <option value={"celebs"}>Celebs</option>
