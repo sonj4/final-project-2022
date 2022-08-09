5829a202fa181d5d1063106de6500e9418a43731
Sonja Knezevic <sonjaknezevic2000@gmail.com>
2022-08-09 16:10:31 +0200

    finished search

--- frontend/src/components/SearchBar/SearchBar.jsx
+++ frontend/src/components/SearchBar/SearchBar.jsx
@@ -2,75 +2,47 @@ import './search-bar.css'
 import { Link } from 'react-router-dom'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { faSearch, faClose } from '@fortawesome/free-solid-svg-icons'
-import { useState, useEffect} from 'react'
+import { useState, useEffect } from 'react'
 import { useNavigate } from 'react-router-dom'
-
+import useAxios from '../../hooks/useAxios';
+import axios from 'axios'
 export default function SearchBar() {
-    const movieData = [{
-        title: "interstellar"
-    }, {
-        title: "forrest gump"
-    }, {
-        title: "idk"
-    }, {
-        title: "interstellar"
-    }, {
-        title: "interstellar"
-    }, {
-        title: "interstellar"
-    }]
 
-    const tvShowsData = [{
-        title: "stranger things"
-    }, {
-        title: "peaky blinders"
-    }, {
-        title: "squid game"
-    }, {
-        title: "twin peaks"
-    }, {
-        title: "breaking bad"
-    }, {
-        title: "the offiCe"
-    }]
-
-    const celebData = [{
-        title: "Millie Bobby Brown"
-    }, {
-        title: "Noah Schnapp"
-    }, {
-        title: "Fin Wolfhard"
-    }, {
-        title: "Jimmy Fallon"
-    }, {
-        title: "Harry Styles"
-    }, {
-        title: "Tom Oddel"
-    }]
 
     const [filteredData, setFilteredData] = useState([]);
     const [wordEntered, setWordEntered] = useState("");
     const [isOnFocus, setIsOnFocus] = useState(false);
     const [searchValue, setSearchValue] = useState("");
+    const [data, setData] = useState([]);
     let navigate = useNavigate();
+
+    useEffect(() => {
+        if (searchValue) {
+            if (searchValue === "movies") {
+                axios.get('https://djangoproject133.herokuapp.com/api/movie/movie/').then(res => setData(res.data));
+            }
+
+            else if (searchValue === "celebs") axios.get('https://djangoproject133.herokuapp.com/api/movie/participant/').then(res => setData(res.data));
+        }
+    }, [searchValue])
 
     const handleFilter = (event) => {
         const searchWord = event.target.value;
         setWordEntered(searchWord);
-        let data = [];
-        if (searchValue === "movies") data = [...movieData];
-        else if (searchValue === "tvshows") data = [...tvShowsData];
-        else if (searchValue === "celebs") data = [...celebData];
         const newFilter = data.filter((value) => {
-            return value.title.toLowerCase().includes(searchWord.toLowerCase());
+            if (searchValue === "movies")
+                return value.title.toLowerCase().includes(searchWord.toLowerCase());
+            else return value.name.toLowerCase().includes(searchWord.toLowerCase());
         });
 
         if (searchWord === "") {
             setFilteredData([]);
         } else {
+            console.log(newFilter)
             setFilteredData(newFilter);
         }
     };
+
 
     const clearInput = () => {
         setFilteredData([]);
@@ -80,26 +52,26 @@ export default function SearchBar() {
     return (
         <div className="input-container">
             <div className="searchMainContainer">
-                <select className='searchBySelect' onChange={e => {setSearchValue(e.target.value); console.log(e.target.value)}}>
-                    <option value={"none"}  defaultValue={true}>Search by</option>
+                <select className='searchBySelect' onChange={e => { setSearchValue(e.target.value); console.log(e.target.value) }}>
+                    <option value={"none"} defaultValue={true}>Search by</option>
                     <option value={"movies"}>Movies</option>
                     <option value={"tvshows"}>TV shows</option>
-                    <option value={"celebs"}>Celebs</option>
+                    <option value={"celebs"}>Actors</option>
                 </select>
-                <input 
-                type="text" 
-                placeholder='Search' 
-                style={{ color: "white", width: "100%" }} 
-                onChange={handleFilter} value={wordEntered} 
-                onFocus={() => setIsOnFocus(true)} 
-                onBlur={()=>setTimeout(() => {
-                    setIsOnFocus(false)
-                }, 1000) }
-                onKeyUp = {e => {
-                    if (e.key === "Enter") {
-                        navigate('/search-results', {state: {data: filteredData}})
-                    }
-                }}
+                <input
+                    type="text"
+                    placeholder='Search'
+                    style={{ color: "white", width: "100%" }}
+                    onChange={handleFilter} value={wordEntered}
+                    onFocus={() => setIsOnFocus(true)}
+                    onBlur={() => setTimeout(() => {
+                        setIsOnFocus(false)
+                    }, 1000)}
+                    onKeyUp={e => {
+                        if (e.key === "Enter") {
+                            navigate('/search-results', { state: { data: filteredData, type: searchValue } })
+                        }
+                    }}
                 />
 
                 {filteredData.length === 0 ? (
@@ -110,7 +82,9 @@ export default function SearchBar() {
             </div>
             {(filteredData.length !== 0 && isOnFocus) && <div className="searchResults">
                 {filteredData.map(el => {
-                    return (<Link to='/movie/1' className='searchResultItem' target={'_blank'}> <p>{el.title}</p></Link>)
+                    if (searchValue === 'movies') 
+                        return <Link to={`/movie/${el.url.substr(el.url.length - 2).substr(0, 1)}`} className='searchResultItem' target={'_blank'}> <p>{el.title}</p></Link>
+                    else return  <Link to={`/actor/${el.url.substr(el.url.length - 2).substr(0, 1)}`} className='searchResultItem' target={'_blank'}> <p>{el.name + " " + el.last_name}</p></Link>
                 })}
             </div>}
         </div>
--- frontend/src/pages/Search/Search.jsx
+++ frontend/src/pages/Search/Search.jsx
@@ -1,24 +1,19 @@
 import './search.css'
-import { useLocation } from 'react-router-dom';
+import { useLocation, Link } from 'react-router-dom';
 
 
 export default function Search() {
   const location = useLocation();
-  //let data = location.state.data;
+  let data = location.state.data;
+  let type = location.state.type;
   return (
     <div className='searchPageContainer'>
-      {/* {data.map(el => {
-        return (
-          <div> {el.title}</div>
-        )
-      })} */}
-      <div>aaaa</div>
-      <div>aaaa</div>
-      <div>aaaa</div>
-      <div>aaaa</div>
-      <div>aaaa</div>
-      <div>aaaa</div>
-      <div>aaaa</div>
+      {data.map(el => {
+      return(  <div className="searchResultItemm">
+          <Link to={type==="movies" ? `/movie/${el.url.substr(el.url.length - 2).substr(0, 1)}` : `/actor/${el.url.substr(el.url.length - 2).substr(0, 1)}`}><img className='searchResultImmg' src={type==="movies" ? el.image_of_movie : el.image_of_participant} alt="image" /></Link> 
+          <p>{type==="movies" ? el.title:el.name + " " + el.last_name}</p>
+        </div>)
+      })}
     </div>
   )
 }
